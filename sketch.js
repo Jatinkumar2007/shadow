718f43e62f9dddfc4ee1132f2f5bbf4022f907b4
Jatinkumar2007 <83221814+Jatinkumar2007@users.noreply.github.com>
2021-09-16 15:47:29 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -10,7 +10,7 @@ var destiny
 var wizard1,wizard2
 var fire
 var thunder
-
+var attack=0
 function preload(){
  destinyImage=loadImage("shadow fighter.jpg")
 backgroundImage=loadImage("background1.jpg")
@@ -18,8 +18,15 @@ wizard1Image=loadImage("fire wizard.jpg")
 wizard2Image=loadImage("thunder wizard.jpg")
 fireImage=loadImage("fire.png")
 thunderImage=loadImage("thunder.jpg")
-destinyhealthImage=loadImage("healthbar.jpg")
+destinyhealthImage=loadAnimation("hb6.png")
 enemyhealthImage=loadImage("healthbar2.png")
+healthbrImage=loadImage("hb1.png")
+healthbr2Image=loadImage("hb2.png")
+healthbr3Image=loadImage("hb3.png")
+healthbr4Image=loadImage("hb4.png")
+healthbr5Image=loadImage("hb5.png")
+healthbr6Image=loadImage("hb6.png")
+
 }
 function setup() {
   createCanvas(displayWidth,displayHeight);
@@ -31,7 +38,13 @@ function setup() {
   bg.velocityX=-1
   bg.x=bg.width/2
 destinyhealth=createSprite(width-140,35)
-destinyhealth.addImage("healthbar",destinyhealthImage)
+destinyhealth.addAnimation("healthbar",destinyhealthImage)
+destinyhealth.addAnimation("healthbar",healthbrImage)
+destinyhealth.addAnimation("healthbar",healthbr2Image)
+destinyhealth.addAnimation("healthbar",healthbr3Image)
+destinyhealth.addAnimation("healthbar",healthbr4Image)
+destinyhealth.addAnimation("healthbar",healthbr5Image)
+destinyhealth.addAnimation("healthbar",healthbr6Image)
 destinyhealth.scale=0.6
   destiny=createSprite(width/8-20,height/2,50,50);
   destiny.addImage("shadow fighter",destinyImage)
@@ -51,6 +64,8 @@ wizard2.setCollider("rectangle",0,0,1200,900)
 enemyhealth=createSprite(width-140,135)
 enemyhealth.addImage("healthbar2",enemyhealthImage)
 enemyhealth.scale=0.7
+fireGroup=new Group()
+thunderGroup=new Group()
 
 }
 
@@ -81,6 +96,7 @@ if(destiny.isTouching(wizard1)){
 if(destiny.isTouching(wizard2)){
   thunderAttack()
 }
+destinyhb()
   drawSprites();
 }
 
@@ -90,6 +106,7 @@ function fireAttack(){
     fire.addImage(fireImage)
     fire.scale=0.05
     fire.velocityX=-3
+    fireGroup.add(fire)
   }
 
 
@@ -102,7 +119,34 @@ thunder=createSprite(width/2,height/2-20,25,25)
 thunder.addImage(thunderImage)
 thunder.scale=0.2
 thunder.velocityX=-2
+thunderGroup.add(thunder)
 }
 
 }
 
+function destinyhb(){
+if(thunderGroup.isTouching(destiny)){
+attack=attack+1
+if(attack!==0){
+  switch (attack){
+    case 1:destinyhealth.changeAnimation("healthbar",healthbrImage)
+      break;
+      case 2:destinyhealth.changeAnimation("healthbar",healthbr2Image)
+      break;
+      case 3:destinyhealth.changeAnimation("healthbar",healthbr3Image)
+      break;
+      case 4:destinyhealth.changeAnimation("healthbar",healthbr4Image)
+      break;
+      case 5:destinyhealth.changeAnimation("healthbar",healthbr5Image)
+      break;
+case 6:destinyhealth.changeAnimation("healthbar",healthbr6Image)
+break;
+default :text("Game Ended",width/2,height/2)
+break;
+
+
+  }
+}
+}
+
+}
